04923ab0dc2c914017abd136c77a47f7cfa8a6ee
itachi2927 <nguyengiacatluong92@gmail.com>
2022-12-20 21:42:06 +0700

    accessmodifier

---
+++ src/Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+    private double radius;
+    private String color;
+    public Circle(){
+        this.radius = 1.0;
+        this.color = "blue";
+    };
+    public Circle(double radius){
+        this.radius = radius;
+    };
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public double getArea(){
+        return this.radius * this.radius * Math.PI;
+    }
+}
